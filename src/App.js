bb8ceb4df7ec97663810502b820022b9129576c5
tenthin <tenthinten@gmail.com>
2025-10-04 22:45:09 -0400

    i have added add button and delete button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [task, setTask] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() === "") return;
+    setTask([...task, text]);
+    setText("");
+  };
+
+  const handleDelete = (indexToDelete) => {
+    setTask(task.filter((_, index) => index !== indexToDelete));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>To Do App</h1>
+      </div>
+
+      <form onSubmit={handleSubmit} className="list">
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <div className="task">
+        <ol>
+          {task.map((item, index) => (
+            <li key={index}>
+              {item}a
+              <button onClick={() => handleDelete(index)}>X</button>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
