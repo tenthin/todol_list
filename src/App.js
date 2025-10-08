e8a664d0d489c8c28eaaaebb667ab870710ee514
tenthin <tenthinten@gmail.com>
2025-10-08 12:39:56 -0400

    added local storage to store the data in the local storage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import { useState } from "react";
+
+
+import { useState, useEffect} from "react";
 import "./App.css";
 
 function App() {
+  // At the very top of App.js
+console.log("LocalStorage on load:", localStorage.getItem("tasks"));
   const [text, setText] = useState("");
   const [task, setTask] = useState([]);
+
+  //Load task from localstorage when the app starts
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    setTask(savedTasks);
+  }, []);
+
+  // Save tasks to localStorage whenever task changes:
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(task));
+  },[task]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text.trim() === "") return;
-    setTask([...task, text]);
+    setTask([...task, { text, done: false }]);
     setText("");
   };
 
   const handleDelete = (indexToDelete) => {
     setTask(task.filter((_, index) => index !== indexToDelete));
   };
+
+  const handleToggle = (index) => {
+    const updatedTasks = task.map((item, i) =>
+      i === index ? { ...item, done: !item.done } : item
+    );
+    setTask(updatedTasks);
+  };
+
+  const handleClearAll = () =>{
+    setTask([]);
+  }
 
   return (
     <div className="App">
@@ -30,11 +56,21 @@ function App() {
         />
         <button type="submit">Add</button>
       </form>
+      <div className="clear">
+        <button onClick={handleClearAll}>Clear All</button>
+      </div>
       <div className="task">
         <ol>
           {task.map((item, index) => (
-            <li key={index}>
-              {item}a
+            <li
+              key={index}
+              style={{
+                textDecoration: item.done ? "line-through" : "none",
+                color: item.done ? "gray" : "black",
+              }}
+            >
+              {item.text}
+              <button onClick={() => handleToggle(index)}>Done</button>
               <button onClick={() => handleDelete(index)}>X</button>
             </li>
           ))}
